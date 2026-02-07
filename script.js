73861388bd68b1e16c75ec292e4a5b3d5cfc9872
Sephydev <nicolas.bussiere@outlook.fr>
2026-02-07 15:27:54 +0100

    feat(library): Implement book storage

--- script.js
+++ script.js
@@ -1,1 +1,21 @@
-console.log('Hello World!');
+const myLibrary = [];
+
+function Book(title, author, publicationDate) {
+  if (!new.target) {
+    throw Error("Please use 'new' with constructor");
+  }
+
+  this.title = title;
+  this.author = author;
+  this.publicationDate = publicationDate;
+  this.id = crypto.randomUUID();
+}
+
+function addBookToLibrary(title, author, publicationDate) {
+  const book = new Book(title, author, publicationDate);
+  myLibrary.push(book);
+}
+
+addBookToLibrary("Harry Potter", "J.K. Rowling", 1997);
+addBookToLibrary("Lord Of The Ring", "J.R.R. Tolkien", 1954)
+console.log(myLibrary);
